7f1a62a9e45630e31e200f6c4086b573cc729063
dizzie <dizziefalls@gmail.com>
2022-12-20 10:17:49 -0600

    Adds placeholder page at root

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ const server = express();
 
 server.use(morgan('dev'))
 server.use(express.json())
+server.get('/', (req, res, next) => {
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Juicebox API</title>
+  </head>
+  <body>
+    <h1>Just a placeholder for the juicebox API docs</h1>
+  </body>
+  </html>`)
+})
 server.use('/api', apiRouter)
 
 server.listen(PORT, () => {
